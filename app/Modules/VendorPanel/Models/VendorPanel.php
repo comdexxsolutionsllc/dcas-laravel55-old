1e168744a0726c7972b3aa75402df77e374c9d49
Sarah Renner <sarah@sarahrenner.work>
2017-10-20 17:34:58 -0500

    initial VendorPortal module commit

---
+++ app/Modules/VendorPanel/Models/VendorPanel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\VendorPanel\Models;
+
+use App\Model;
+
+class VendorPanel extends Model
+{
+    //
+}
---
+++ app/Modules/VendorPanel/routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+Route::group(['module' => 'VendorPanel', 'prefix' => 'VendorPanel', 'middleware' => ['api'], 'namespace' => 'Modules\VendorPanel\Controllers'], function() {
+
+    Route::resource('VendorPanel', 'VendorPanelController');
+
+});
