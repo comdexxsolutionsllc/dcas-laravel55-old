0b45e7a0f076ae2bc5e848564532da2ed23ac207
Sarah Renner <sarah@sarahrenner.work>
2017-10-25 23:31:37 -0500

    update App\User with EntrustUserTrait::ability() override to use may() instead of can()

--- app/User.php
+++ app/User.php
@@ -249,4 +249,69 @@ class User extends Authenticatable implements Presentable, TwoFactorAuthenticata
     {
         return $this->hasMany(Ticket::class);
     }
+
+    /**
+     * @param $roles
+     * @param $permissions
+     * @param array $options
+     * @return array|bool
+     */
+    public function ability($roles, $permissions, $options = [])
+    {
+        // Convert string to array if that's what is passed in.
+        if (!is_array($roles)) {
+            $roles = explode(',', $roles);
+        }
+        if (!is_array($permissions)) {
+            $permissions = explode(',', $permissions);
+        }
+
+        // Set up default values and validate options.
+        if (!isset($options['validate_all'])) {
+            $options['validate_all'] = false;
+        } else {
+            if ($options['validate_all'] !== true && $options['validate_all'] !== false) {
+                throw new InvalidArgumentException();
+            }
+        }
+        if (!isset($options['return_type'])) {
+            $options['return_type'] = 'boolean';
+        } else {
+            if ($options['return_type'] != 'boolean' &&
+                $options['return_type'] != 'array' &&
+                $options['return_type'] != 'both') {
+                throw new InvalidArgumentException();
+            }
+        }
+
+        // Loop through roles and permissions and check each.
+        $checkedRoles = [];
+        $checkedPermissions = [];
+        foreach ($roles as $role) {
+            $checkedRoles[$role] = $this->hasRole($role);
+        }
+        foreach ($permissions as $permission) {
+            $checkedPermissions[$permission] = $this->may($permission);
+        }
+
+        // If validate all and there is a false in either
+        // Check that if validate all, then there should not be any false.
+        // Check that if not validate all, there must be at least one true.
+        if (($options['validate_all'] && !(in_array(false, $checkedRoles) || in_array(false, $checkedPermissions))) ||
+            (!$options['validate_all'] && (in_array(true, $checkedRoles) || in_array(true, $checkedPermissions)))) {
+            $validateAll = true;
+        } else {
+            $validateAll = false;
+        }
+
+        // Return based on option
+        if ($options['return_type'] == 'boolean') {
+            return $validateAll;
+        } elseif ($options['return_type'] == 'array') {
+            return ['roles' => $checkedRoles, 'permissions' => $checkedPermissions];
+        } else {
+            return [$validateAll, ['roles' => $checkedRoles, 'permissions' => $checkedPermissions]];
+        }
+
+    }
 }
