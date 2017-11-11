881ac58b43a08cd5e3ffc8021737252dc0eabd22
Sarah Renner <sarah@sarahrenner.work>
2017-11-10 22:24:22 -0600

    speed up tests by lowering the default crypt cost factor

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use Hash;
 use Illuminate\Contracts\Console\Kernel;
 
 trait CreatesApplication
@@ -16,6 +17,9 @@ trait CreatesApplication
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
+
+        /* Lower the default crypt cost factor */
+        Hash::setRounds(5);
 
         return $app;
     }
