5c37d4275cbb506b8bc51e173648948f5fb2c930
Sarah Renner <sarah@sarahrenner.work>
2017-12-24 19:22:16 -0600

    update SupportDesk controllers

---
+++ app/Modules/SupportDesk/Controllers/CategoryController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Modules\SupportDesk\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Modules\SupportDesk\Models\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \Modules\SupportDesk\Models\Category $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \Modules\SupportDesk\Models\Category $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Modules\SupportDesk\Models\Category $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Modules\SupportDesk\Models\Category $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
---
+++ app/Modules/SupportDesk/Controllers/StatusController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Modules\SupportDesk\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Modules\SupportDesk\Models\Status;
+
+class StatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \Modules\SupportDesk\Models\Status $status
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Status $status)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \Modules\SupportDesk\Models\Status $status
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Status $status)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Modules\SupportDesk\Models\Status $status
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Status $status)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Modules\SupportDesk\Models\Status $status
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Status $status)
+    {
+        //
+    }
+}
